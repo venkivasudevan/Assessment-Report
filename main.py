1b45746c5306ea918b84354ec64bef4b8b3803fc
Lakshmivenkateshwaran <vvenkivasudevan9@gmail.com>
2024-09-16 16:28:49 +0530

    Python file has been added

---
+++ main.py
@@ -0,0 +1,17 @@
+import pdfkit
+
+path_to_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+
+config = pdfkit.configuration(wkhtmltopdf=path_to_wkhtmltopdf)
+
+# Path to your HTML file
+html_file = 'index.html'
+css_file = r'C:\Users\venki\Work\Compunet_Connections\Frontend\css\style.css'
+
+# Output PDF file name
+final_report = 'final_report.pdf'
+
+# Convert HTML to PDF
+pdfkit.from_file(html_file, final_report, configuration=config, css=css_file)
+
+print(f'HTML file {html_file} has been successfully converted to {final_report}')
